f13737e0f33076d8432329d701d93d60537d494a
Matt Walker <matt@my-html-codes.com>
2013-07-03 13:06:55 +0100

    Release number 9

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -2,7 +2,15 @@
 stylesheet 'screen.scss', :media => 'screen, projection'
 stylesheet '_base.scss'
 
-description "Respondsass: a fully configurable grid system native to compass."
+file 'scripts/boxsizing.htc', :like => :javascript, :to => 'boxsizing.htc'
+file 'scripts/html5shiv.js', :like => :javascript, :to => 'html5shiv.js'
+file 'scripts/modernizr.custom.js', :like => :javascript, :to => 'modernizr.custom.js'
+file 'scripts/respond.min.js', :like => :javascript, :to => 'respond.min.js'
+
+html 'index.html'
+html 'examples.html'
+
+description "Respondsass: a fully configurable grid system native to Compass."
 
 help %Q{
 Please see the Respondsass website for all documentation and tutorials:
